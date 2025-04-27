b63900d177d46c0ada979dd43d0ff44e7bc7f410
Mohammad Al-Ahdal <hackr@hackr.sh>
2025-04-27 10:54:25 -0600

    update niri configuration: enable Niri with the latest unstable package and force the default session to Niri

--- modules/nixos/niri.nix
+++ modules/nixos/niri.nix
@@ -1,13 +1,15 @@
-{ pkgs, ... }:
+{ pkgs, lib, inputs, ... }:
 {
   # Enable Niri
   programs.niri = {
     enable = true;
   };
+  # use the latest push to the main branch for niri, comment out in case stuff breaks
+  programs.niri.package = inputs.niri-unstable.packages.${pkgs.system}.niri;
 
   environment.systemPackages = with pkgs; [
     xwayland-satellite
   ];
 
-  services.displayManager.defaultSession = "niri";
+  services.displayManager.defaultSession = lib.mkForce "niri";
 }
