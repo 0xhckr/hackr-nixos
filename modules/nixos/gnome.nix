732ecb0fa59a0c684cba63da300840f53aebc641
Mohammad Al-Ahdal <hackr@hackr.sh>
2025-04-27 10:53:23 -0600

    add gnome configuration: enable X server, GDM, and GNOME desktop manager, and disable getty and autovt services

---
+++ modules/nixos/gnome.nix
@@ -0,0 +1,9 @@
+{ ... }:
+{
+  services.xserver.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  systemd.services."getty@tty1".enable = false;
+  systemd.services."autovt@tty1".enable = false;
+}
