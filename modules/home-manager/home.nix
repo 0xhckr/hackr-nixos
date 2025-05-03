96c8c926f3960d0c3f839d7878f99f4d2e497b5f
Mohammad Al-Ahdal <hackr@hackr.sh>
2025-05-03 08:47:53 -0600

    refactor home.nix by removing unnecessary comments for clarity and maintaining the home.stateVersion setting

--- modules/home-manager/home.nix
+++ modules/home-manager/home.nix
@@ -5,10 +5,7 @@
   system,
   ...
 }:
-
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   home.username = "hackr";
   home.homeDirectory = "/home/hackr";
   home.shell.enableNushellIntegration = true;
@@ -19,17 +16,8 @@
     ./apps.nix
   ];
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "24.11"; # Please read the comment before changing.
+  home.stateVersion = "24.11";
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
   home.packages = with pkgs; [
     nil
     nixfmt-rfc-style
