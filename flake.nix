2d8f055268d2d70b4f2d4c4cae161b06d7217a01
Mohammad Al-Ahdal <hackr@hackr.sh>
2025-06-06 16:03:01 -0600

    add nix-vscode-extensions and update systems version in flake.lock

--- flake.nix
+++ flake.nix
@@ -81,6 +81,11 @@
       ref = "main";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    nix-vscode-extensions = {
+      url = "github:nix-community/nix-vscode-extensions";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
