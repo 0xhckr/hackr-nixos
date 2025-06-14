eba9bd25ae042b3772fc427d871a3cd0d06f614d
Mohammad Al-Ahdal <hackr@hackr.sh>
2025-06-13 22:35:04 -0600

    Enhance Podman configuration in NixOS by enabling Docker compatibility and DNS settings; add additional system packages: dive, podman-tui, and podman-compose.

--- modules/nixos/docker.nix
+++ modules/nixos/docker.nix
@@ -5,6 +5,16 @@
 }:
 
 {
-  virtualisation.podman.enable = true;
+  virtualisation.podman = {
+    enable = true;
+    dockerCompat = true;
+    defaultNetwork.settings.dns_enabled = true;
+  };
   users.users.hackr.extraGroups = [ "docker" ];
+
+  environment.systemPackages = with pkgs; [
+    dive
+    podman-tui
+    podman-compose
+  ];
 }
