273900084a56b51b8a46e35681040481c5f1234c
Mohammad Al-Ahdal <hackr@hackr.sh>
2025-07-21 09:59:20 -0600

    Refactor GNOME service configuration in NixOS to streamline display manager and desktop manager settings

--- modules/nixos/gnome.nix
+++ modules/nixos/gnome.nix
@@ -1,8 +1,8 @@
 { ... }:
 {
   services.xserver.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
 
   systemd.services."getty@tty1".enable = false;
   systemd.services."autovt@tty1".enable = false;
