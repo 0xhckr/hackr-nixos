f55066e9759f49e79f938e42c2cf53dbc73122f0
Mohammad Al-Ahdal <hackr@hackr.sh>
2025-08-03 15:31:05 -0600

    add nur overlay

--- modules/nixos/defaults.nix
+++ modules/nixos/defaults.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ inputs, ... }:
 {
   boot.loader.efi.canTouchEfiVariables = true;
 
@@ -21,7 +21,17 @@
         doCheck = false;
       });
     })
+    inputs.nur.overlays.default
   ];
+
+  nixpkgs.config.packageOverrides = pkgs: {
+    nur = import (builtins.fetchTarball {
+      url = "https://github.com/nix-community/NUR/archive/6f2328a65015c49b8d571f7e64867edd107c854c.tar.gz";
+      sha256 = "15njf1z2s5hirmymlydy5hc8isvd181nkv7irid74lf236f8kxjz";
+    }) {
+      inherit pkgs;
+    };
+  };
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
