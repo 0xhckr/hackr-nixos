ebb21d1a0731378e7a7dbe5e48dda936e05c3b40
Mohammad Al-Ahdal <hackr@hackr.sh>
2025-09-14 17:46:21 -0600

    fix cursor not launching since display env is not set

--- modules/home-manager/terminal.nix
+++ modules/home-manager/terminal.nix
@@ -83,6 +83,7 @@ in
             $env.config.show_banner = false
             $env.DIRENV_LOG_FORMAT = ""
             $env.NIXPKGS_ALLOW_UNFREE = "1"
+            $env.DISPLAY = ":0"
           '';
         };
       }
