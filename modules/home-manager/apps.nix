751fd12977520e020537adddd999d9c6e8a5ba9b
Mohammad Al-Ahdal <hackr@hackr.sh>
2025-09-25 20:36:58 -0600

    yay discord noise suppression :D

--- modules/home-manager/apps.nix
+++ modules/home-manager/apps.nix
@@ -43,6 +43,27 @@ let
         zen-browser.packages."${system}".default
       ]
     ));
+  krisp-patcher =
+    pkgs.writers.writePython3Bin "krisp-patcher"
+      {
+        libraries = with pkgs.python3Packages; [
+          capstone
+          pyelftools
+        ];
+        flakeIgnore = [
+          "E501" # line too long (82 > 79 characters)
+          "F403" # 'from module import *' used; unable to detect undefined names
+          "F405" # name may be undefined, or defined from star imports: module
+        ];
+      }
+      (
+        builtins.readFile (
+          pkgs.fetchurl {
+            url = "https://pastebin.com/raw/8tQDsMVd";
+            sha256 = "sha256-IdXv0MfRG1/1pAAwHLS2+1NESFEz2uXrbSdvU9OvdJ8=";
+          }
+        )
+      );
 in
 {
   imports = [
@@ -56,6 +77,7 @@ in
       obsidian
       fontforge
       discord # discord now works flawlessly on NixOS. no need for vesktop.
+      krisp-patcher
     ]
     ++ (lib.optionals isLinux [
       zoom-us
