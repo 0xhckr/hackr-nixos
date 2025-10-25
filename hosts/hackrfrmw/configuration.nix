7b101c54ef467d16684b78bcb356f6e3d424a951
Mohammad Al-Ahdal <hackr@hackr.sh>
2025-10-25 04:10:06 -0600

    fix broken audio on laptop (kind of)

--- hosts/hackrfrmw/configuration.nix
+++ hosts/hackrfrmw/configuration.nix
@@ -57,6 +57,12 @@
     };
   };
 
+  services.pipewire.wireplumber.extraConfig.no-ucm = {
+    "monitor.alsa.properties" = {
+      "alsa.use-ucm" = false;
+    };
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
