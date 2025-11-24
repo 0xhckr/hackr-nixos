4651d8fe7cfd802bf5abc9655db1ba901e4a38de
Mohammad Al-Ahdal <hackr@hackr.sh>
2025-11-24 04:10:41 -0700

    Revert "unify the bootloaders"
    This reverts commit 6b83217ecd28355baf6cad231f5fe6a3e9095142.

--- hosts/hackrpc/boot.nix
+++ hosts/hackrpc/boot.nix
@@ -1,11 +1,63 @@
 { pkgs, lib, ... }:
 {
-  imports = [
-    ../../modules/nixos/unified-boot.nix
+  environment.systemPackages = with pkgs; [
+    sbctl
   ];
 
-  boot.loader.limine = {
-    secureBoot.enable = true;
-    style.interface.resolution = "5120x2160";
+  boot = {
+    loader = {
+      systemd-boot =  {
+        enable = lib.mkForce false;
+        consoleMode = "max";
+      };
+      limine = {
+        enable = true;
+        secureBoot.enable = true;
+        extraConfig = ''
+term_palette: 1e1e2e;f38ba8;a6e3a1;f9e2af;89b4fa;f5c2e7;94e2d5;cdd6f4
+term_palette_bright: 585b70;f38ba8;a6e3a1;f9e2af;89b4fa;f5c2e7;94e2d5;cdd6f4
+term_background: 1e1e2e
+term_foreground: cdd6f4
+term_background_bright: 585b70
+term_foreground_bright: cdd6f4
+        '';
+        extraEntries = ''
+          /+Windows
+          //Windows
+            protocol: efi
+            path: boot():/EFI/Microsoft/Boot/bootmgfw.efi
+        '';
+        style = {
+          wallpapers = [];
+          interface = {
+            resolution = "5120x2160";
+            helpHidden = true;
+            branding = "";
+          };
+          backdrop = "1E1E2E";
+        };
+      };
+      timeout = 5;
+    };
+    plymouth = {
+      enable = true;
+      theme = lib.mkForce "dark_planet";
+      themePackages = with pkgs; [
+        (adi1090x-plymouth-themes.override {
+          selected_themes = [ "dark_planet" ];
+        })
+      ];
+    };
+    consoleLogLevel = 3;
+    initrd = {
+      verbose = false;
+    };
+    kernelParams = [
+      "quiet"
+      "splash"
+      "boot.shell_on_fail"
+      "udev.log-priority=3"
+      "rd.systemd.show_status=auto"
+    ];
   };
 }
--- hosts/hackrwork/boot.nix
+++ hosts/hackrwork/boot.nix
@@ -4,9 +4,6 @@
     # For debugging and troubleshooting Secure Boot.
     pkgs.sbctl
   ];
-  imports = [
-    ../../modules/nixos/unified-boot.nix
-  ];
 
-  boot.loader.limine.style.interface.resolution = "3840x2160";
+  boot.loader.systemd-boot.enable = true;
 }
