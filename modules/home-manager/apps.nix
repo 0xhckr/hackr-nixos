08149c7626a7fded34fe4e1335b778c6a1dd2b0b
Mohammad Al-Ahdal <hackr@hackr.sh>
2025-12-29 09:43:21 -0700

    add helix config with language servers etc

--- modules/home-manager/apps.nix
+++ modules/home-manager/apps.nix
@@ -4,58 +4,50 @@
   system,
   lib,
   ...
-}:
-let
-  jetbrainsApps =
-    with pkgs.jetbrains;
-    [
-      datagrip
-      rider
-      rust-rover
-      idea-ultimate
-    ];
+}: let
+  jetbrainsApps = with pkgs.jetbrains; [
+    datagrip
+    rider
+    rust-rover
+    idea-ultimate
+  ];
   # NOTE: requires manual download of affinity apps.
-  affinityApps =
-    with inputs.affinity-nix.packages."${system}";
-    [
-      v3
-    ];
-  browsers =
-    with pkgs;
-    [
-      ungoogled-chromium
-      firefox
-      inputs.zen-browser.packages."${system}".default
-    ];
+  affinityApps = with inputs.affinity-nix.packages."${system}"; [
+    v3
+  ];
+  browsers = with pkgs; [
+    ungoogled-chromium
+    firefox
+    inputs.zen-browser.packages."${system}".default
+  ];
   krisp-patcher =
     pkgs.writers.writePython3Bin "krisp-patcher"
-      {
-        libraries = with pkgs.python3Packages; [
-          capstone
-          pyelftools
-        ];
-        flakeIgnore = [
-          "E501" # line too long (82 > 79 characters)
-          "F403" # 'from module import *' used; unable to detect undefined names
-          "F405" # name may be undefined, or defined from star imports: module
-        ];
-      }
-      (
-        builtins.readFile (
-          pkgs.fetchurl {
-            url = "https://pastebin.com/raw/8tQDsMVd";
-            sha256 = "sha256-IdXv0MfRG1/1pAAwHLS2+1NESFEz2uXrbSdvU9OvdJ8=";
-          }
-        )
-      );
-in
-{
+    {
+      libraries = with pkgs.python3Packages; [
+        capstone
+        pyelftools
+      ];
+      flakeIgnore = [
+        "E501" # line too long (82 > 79 characters)
+        "F403" # 'from module import *' used; unable to detect undefined names
+        "F405" # name may be undefined, or defined from star imports: module
+      ];
+    }
+    (
+      builtins.readFile (
+        pkgs.fetchurl {
+          url = "https://pastebin.com/raw/8tQDsMVd";
+          sha256 = "sha256-IdXv0MfRG1/1pAAwHLS2+1NESFEz2uXrbSdvU9OvdJ8=";
+        }
+      )
+    );
+in {
   imports = [
     inputs._1password.hmModules.default
     ./apps/cursor.nix
+    ./apps/helix
   ];
-  home.packages =
-    with pkgs;
+  home.packages = with pkgs;
     [
       spotify
       obsidian
@@ -79,17 +71,17 @@ in
     ++ affinityApps
     ++ browsers;
 
-    # needed for vicinae to properly launch x apps in niri
-    systemd.user.settings.Manager.DefaultEnvironment = {
-      DISPLAY = ":0";
-      XAUTHORITY = "$HOME/.Xauthority";
-    };
+  # needed for vicinae to properly launch x apps in niri
+  systemd.user.settings.Manager.DefaultEnvironment = {
+    DISPLAY = ":0";
+    XAUTHORITY = "$HOME/.Xauthority";
+  };
 
-    programs._1password-shell-plugins = {
-      enable = true;
-    };
+  programs._1password-shell-plugins = {
+    enable = true;
+  };
 
-    programs.zsh = {
-      enable = true;
-    };
+  programs.zsh = {
+    enable = true;
+  };
 }
---
+++ modules/home-manager/apps/helix/default.nix
@@ -0,0 +1,72 @@
+{
+  pkgs,
+  inputs,
+  ...
+}: let
+  formatters = with pkgs; [
+    # nix
+    alejandra
+    # rust
+    rustfmt
+    dioxus-cli
+    # zig
+    zig
+    # yaml
+    yamlfmt
+  ];
+
+  languageServers = with pkgs; [
+    # rust
+    rust-analyzer
+    # nix
+    nil
+    # typescript + javascript + tsx + jsx
+    typescript-language-server
+    # json + jsonc
+    vscode-json-languageserver
+    # go
+    gopls
+    golangci-lint-langserver
+    # bash
+    bash-language-server
+    # astro
+    astro-language-server
+    # c-sharp
+    csharp-ls
+    omnisharp-roslyn
+    # yaml
+    yaml-language-server
+    # prisma
+    prisma-language-server
+    # zig
+    zls
+    # css
+    vscode-css-languageserver
+    # toml
+    taplo
+    tombi
+  ];
+
+  debuggers = with pkgs; [
+    # go
+    delve
+    # c-sharp
+    netcoredbg
+    # zig
+    lldb
+  ];
+
+  formattersWithLanguageServers = with pkgs; [
+    # typescript + javascript + tsx + jsx + json + jsonc
+    biome
+  ];
+in {
+  home.packages = with pkgs;
+    [
+      inputs.helix.packages."${system}".default
+    ]
+    ++ debuggers
+    ++ languageServers
+    ++ formatters
+    ++ formattersWithLanguageServers;
+}
