b24cd900245d23c784b64bcfd1a2127832b69636
Mohammad Al-Ahdal <hackr@hackr.sh>
2026-01-15 23:22:03 -0700

    Refactor editor module to language-servers

--- modules/home-manager/apps/editor/default.nix
+++ modules/home-manager/apps/editor/default.nix
@@ -1,16 +1,19 @@
 {
   pkgs,
   ...
-}: {
+}:
+{
   imports = [
-    ./keybindings.nix
-    ./user-config.nix
-    ./extensions.nix
+    ./language-servers.nix
+    # TODO: remove all references to vscode in favour of zed-editor-fhs
+    # ./keybindings.nix
+    # ./user-config.nix
+    # ./extensions.nix
   ];
-  programs.vscode = {
-    enable = true;
-    package = pkgs.vscode-fhs;
-  };
+  # programs.vscode = {
+  #   enable = true;
+  #   package = pkgs.vscode-fhs;
+  # };
 
   home.packages = with pkgs; [
     zed-editor-fhs
