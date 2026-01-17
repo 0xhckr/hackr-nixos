20d79e71f8215c1edced06d53b4281588ad954d6
Mohammad Al-Ahdal <hackr@hackr.sh>
2026-01-17 11:55:16 -0700

    fix(editor): launching from terminal causes environment variables not to load

--- modules/home-manager/nushell.nix
+++ modules/home-manager/nushell.nix
@@ -64,9 +64,9 @@
 
       def code [...args: string] {
         if $args == null or $args == [] {
-          bash -c $"nohup ${pkgs.zed-editor-fhs}/bin/zeditor >/dev/null 2>&1 &"
+          bash -c $"TERM=xterm-256color nohup ${pkgs.zed-editor-fhs}/bin/zeditor >/dev/null 2>&1 &"
         } else {
-          bash -c $"nohup ${pkgs.zed-editor-fhs}/bin/zeditor ($args | str join ' ') >/dev/null 2>&1 &"
+          bash -c $"TERM=xterm-256color nohup ${pkgs.zed-editor-fhs}/bin/zeditor ($args | str join ' ') >/dev/null 2>&1 &"
         }
       }
 
