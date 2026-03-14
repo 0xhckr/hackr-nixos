bc2891b3e8ad0afe6b00cdeeb26cf012df534bba
Mohammad Al-Ahdal <hackr@hackr.sh>
2026-03-14 06:52:01 -0600

    sync jj settings with nix

--- modules/home-manager/ssh/git.nix
+++ modules/home-manager/ssh/git.nix
@@ -4,5 +4,9 @@
       source = ../../../ssh/home.gitconfig;
       force = true;
     };
+    ".config/jj/config.toml" = {
+      source = ../../../ssh/home.jj-config.toml;
+      force = true;
+    };
   };
 }
