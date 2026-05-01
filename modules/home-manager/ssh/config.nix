f32ce2699b605b58468c396a29480124e995e66d
Mohammad Al-Ahdal <hackr@hackr.sh>
2026-04-30 23:51:39 -0600

    Add thundurus to SSH config

--- modules/home-manager/ssh/config.nix
+++ modules/home-manager/ssh/config.nix
@@ -1,6 +1,10 @@
 {username, ...}: {
   home.file.".ssh/config" = {
     text = ''
+      Host thundurus
+        HostName 54.39.157.27
+        User ${username}
+
       Host 10.0.11.5
         HostName 10.0.11.5
         User ${username}
