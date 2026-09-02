35c58ed4ef42383e271712345eb218d97f9b40ac
Mohammad Al-Ahdal <hackr@hackr.sh>
2026-09-02 09:26:59 -0600

    hosts/flareon: switch bootloader from GRUB to Limine

--- hosts/flareon/boot.nix
+++ hosts/flareon/boot.nix
@@ -1,21 +1,7 @@
-{
-  lib,
-  pkgs,
-  ...
-}: {
+{pkgs, ...}: {
   boot = {
     kernelPackages = pkgs.linuxPackages_latest;
-    loader = {
-      efi = {
-        canTouchEfiVariables = lib.mkForce true;
-      };
-      grub = {
-        enable = true;
-        device = "nodev";
-        useOSProber = true;
-        efiSupport = true;
-      };
-    };
+    loader.limine.enable = true;
   };
 
   powerManagement.enable = true;
